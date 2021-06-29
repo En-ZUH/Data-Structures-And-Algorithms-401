eb5f062ca88be2e1c4f3c060569a0adf27adb505
Enas Batayneh <79834615+En-ZUH@users.noreply.github.com>
2021-06-29 16:15:39 +0300

    Update BinarySearch.js

--- javascript/code-challenges/challenge03/BinarySearch.js
+++ javascript/code-challenges/challenge03/BinarySearch.js
@@ -1,1 +1,20 @@
-
+'use strict';
+let midIndex;
+ 
+function BinarySearch (arr,key){
+ let lowIndex=0;
+ let highIndex= arr.length-1;
+ while(lowIndex<=highIndex){
+  midIndex =Math.floor((lowIndex+highIndex)/2);
+  if(arr[midIndex]===key)
+    return midIndex;
+  else if(arr[midIndex]<key)
+    lowIndex=midIndex+1;   
+  else{ 
+    highIndex=midIndex-1;
+ }}
+ return -1;
+}
+let array=[1,2,3,5];
+// BinarySearch(array, 5);
+BinarySearch(array, 20);
