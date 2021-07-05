98a5b5e52017b3facbdccdc95f9177a3bd60d155
En-ZUH <enas_z_bataineh108@yahoo.com>
2021-07-05 20:38:36 +0300

    done

--- javascript/code-challenges/challenge05/linked-list.js
+++ javascript/code-challenges/challenge05/linked-list.js
@@ -1,87 +1,73 @@
 'use strict';
 
 class Node {
-	constructor(value, next = null) {
-		this.value = value;
-		this.next = next;
-	}
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
 }
 
 
 class LinkedList {
-	constructor() {
-		this.head = null;
-	}
+  constructor() {
+    this.head = null;
+  }
 
 
-	insert(value) {
-		try {
+  insert(value) {
+    try {
 
-			if (!value) {
-				throw new Error('Please enter a value');
-			}
-			let newNode = this.head;
-			this.head = new Node(value, newNode);
-			this.length++;
-		}
-		catch (error) {
-			console.log('Error: insert method', error);
-		}
-	}
+      if (!value) {
+        throw new Error('Please enter a value');
+      }
+      let newNode = this.head;
+      this.head = new Node(value, newNode);
+      this.length++;
+    }
+    catch (error) {
+      console.log('Error: insert method', error);
+    }
+  }
 
-	include(value) {
-		try {
+  include(value) {
+    try {
 
-			if (!value) {
-				throw new Error('Please enter a value');
-			}
+      if (!value) {
+        throw new Error('Please enter a value');
+      }
 
-			let current = this.head;
+      let current = this.head;
 
-			while (current) {
-				if (current.value === value) {
-					return true;
-				}
-				current = current.next;
-			}
-			return false;
-		} catch (error) {
-			console.log('Error: includes method', error);
-		}
-	}
-	append(value) {
-		var newNode = new Node(value);
-		this.length++;
-		if (!this.head) {
-			this.head = newNode;
-		} else {
-			let lastNode = this.head;
-			while (lastNode.next) {
-				lastNode = lastNode.next;
-			}
-			lastNode.next = newNode;
-		}
-		return this;
-	}
+      while (current) {
+        if (current.value === value) {
+          return true;
+        }
+        current = current.next;
+      }
+      return false;
+    } catch (error) {
+      console.log('Error: includes method', error);
+    }
+  }
 
 
-	toString() {
-		try {
-			let linkedList = '';
-			let current = this.head;
-			while (current) {
-				if (current.next === null) {
-					linkedList += `{${current.value}} -> NULL`;
-				} else {
-					linkedList += `{ ${current.value} } -> `;
-				}
-				current = current.next;
-			}
-			return linkedList;
-		} catch (error) {
-			console.log(`Error: toString method`, error);
-		}
-	}
+  toString() {
+    try {
+      let linkedList = '';
+      let current = this.head;
+      while (current) {
+        if (current.next === null) {
+          linkedList += `{${current.value}} -> NULL`;
+        } else {
+          linkedList += `{ ${current.value} } -> `;
+        }
+        current = current.next;
+      }
+      return linkedList;
+    } catch (error) {
+      console.log(`Error: toString method`, error);
+    }
+  }
 
 
 }
