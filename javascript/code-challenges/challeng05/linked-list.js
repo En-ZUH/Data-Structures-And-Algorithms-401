7678910fefbfb3b400a1778010477d683b76421f
En-ZUH <enas_z_bataineh108@yahoo.com>
2021-07-05 19:15:21 +0300

    done

--- javascript/code-challenges/challeng05/linked-list.js
+++ javascript/code-challenges/challeng05/linked-list.js
@@ -1,38 +1,89 @@
 'use strict';
 
-const Node = require('./Node');
-const LinkedListErrors = require('./errors/LinkedList');
+class Node {
+	constructor(value, next = null) {
+		this.value = value;
+		this.next = next;
+	}
+}
 
-let linkedListErrors = new LinkedListErrors();
 
 class LinkedList {
 	constructor() {
 		this.head = null;
-		this.length = 0;
 	}
 
-	checkInputs(value) {
-		if (value === undefined || value === Infinity) {
-			throw LinkedListErrors.notDefinedInput(
-				`${value} not valid input`
-			);
-		}
-	}
 
 	insert(value) {
-		this.checkInputs(value);
-		let node = new Node(value);
-		if (!this.head) {
-			this.head = node;
+		try {
+
+			if (!value) {
+				throw new Error('Please enter a value');
+			}
+			let newNode = this.head;
+			this.head = new Node(value, newNode);
 			this.length++;
 		}
-		else {
-			let currentNode = this.head;
-			while (currentNode.next) {
-				currentNode = currentNode.next;
-			}
-			currentNode.next = node;
-			this.length++;
+		catch (error) {
+			console.log('Error: insert method', error);
 		}
 	}
+
+	include(value) {
+		try {
+
+			if (!value) {
+				throw new Error('Please enter a value');
+			}
+
+			let current = this.head;
+
+			while (current) {
+				if (current.value === value) {
+					return true;
+				}
+				current = current.next;
+			}
+			return false;
+		} catch (error) {
+			console.log('Error: includes method', error);
+		}
+	}
+	append(value) {
+		var newNode = new Node(value);
+		this.length++;
+		if (!this.head) {
+			this.head = newNode;
+		} else {
+			let lastNode = this.head;
+			while (lastNode.next) {
+				lastNode = lastNode.next;
+			}
+			lastNode.next = newNode;
+		}
+		return this;
+	}
+
+
+	toString() {
+		try {
+			let linkedList = '';
+			let current = this.head;
+			while (current) {
+				if (current.next === null) {
+					linkedList += `{${current.value}} -> NULL`;
+				} else {
+					linkedList += `{ ${current.value} } -> `;
+				}
+				current = current.next;
+			}
+			return linkedList;
+		} catch (error) {
+			console.log(`Error: toString method`, error);
+		}
+	}
+
+
 }
+module.exports = LinkedList;
+
