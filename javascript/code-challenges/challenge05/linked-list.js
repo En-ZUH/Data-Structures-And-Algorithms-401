5165d7c0bde1928f4986c4864f979480fb8de0da
En-ZUH <enas_z_bataineh108@yahoo.com>
2021-07-05 20:36:24 +0300

    done

--- javascript/code-challenges/challenge05/linked-list.js
+++ javascript/code-challenges/challenge05/linked-list.js
@@ -1,89 +1,132 @@
 'use strict';
 
 class Node {
-	constructor(value, next = null) {
-		this.value = value;
-		this.next = next;
-	}
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
 }
 
 
 class LinkedList {
-	constructor() {
-		this.head = null;
-	}
+  constructor() {
+    this.head = null;
+  }
 
 
-	insert(value) {
-		try {
+  insert(value) {
+    try {
 
-			if (!value) {
-				throw new Error('Please enter a value');
-			}
-			let newNode = this.head;
-			this.head = new Node(value, newNode);
-			this.length++;
-		}
-		catch (error) {
-			console.log('Error: insert method', error);
-		}
-	}
+      if (!value) {
+        throw new Error('Please enter a value');
+      }
+      let newNode = this.head;
+      this.head = new Node(value, newNode);
+      this.length++;
+    }
+    catch (error) {
+      console.log('Error: insert method', error);
+    }
+  }
 
-	include(value) {
-		try {
+  include(value) {
+    try {
 
-			if (!value) {
-				throw new Error('Please enter a value');
-			}
+      if (!value) {
+        throw new Error('Please enter a value');
+      }
 
-			let current = this.head;
+      let current = this.head;
 
-			while (current) {
-				if (current.value === value) {
-					return true;
-				}
-				current = current.next;
-			}
-			return false;
-		} catch (error) {
-			console.log('Error: includes method', error);
-		}
-	}
-	append(value) {
-		var newNode = new Node(value);
-		this.length++;
-		if (!this.head) {
-			this.head = newNode;
-		} else {
-			let lastNode = this.head;
-			while (lastNode.next) {
-				lastNode = lastNode.next;
-			}
-			lastNode.next = newNode;
-		}
-		return this;
-	}
+      while (current) {
+        if (current.value === value) {
+          return true;
+        }
+        current = current.next;
+      }
+      return false;
+    } catch (error) {
+      console.log('Error: includes method', error);
+    }
+  }
+
+  toString() {
+    try {
+      let linkedList = '';
+      let current = this.head;
+      while (current) {
+        if (current.next === null) {
+          linkedList += `{${current.value}} -> NULL`;
+        } else {
+          linkedList += `{ ${current.value} } -> `;
+        }
+        current = current.next;
+      }
+      return linkedList;
+    } catch (error) {
+      console.log(`Error: toString method`, error);
+    }
+
+  }
+
+  append(value) {
+    var newNode = new Node(value);
+    this.length++;
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let lastNode = this.head;
+      while (lastNode.next) {
+        lastNode = lastNode.next;
+      }
+      lastNode.next = newNode;
+    }
+    return this;
+  }
 
 
-	toString() {
-		try {
-			let linkedList = '';
-			let current = this.head;
-			while (current) {
-				if (current.next === null) {
-					linkedList += `{${current.value}} -> NULL`;
-				} else {
-					linkedList += `{ ${current.value} } -> `;
-				}
-				current = current.next;
-			}
-			return linkedList;
-		} catch (error) {
-			console.log(`Error: toString method`, error);
-		}
-	}
 
 
+  insertAfter(value, newVal) {
+    let node = new Node(newVal);
+    let current = this.head;
+    while (current) {
+      if (current.value === value) {
+        this.length++;
+        let temp = current.next;
+        current.next = node;
+        node.next = temp;
+        return;
+      }
+      current = current.next;
+    }
+    return 'exception';
+  }
+
+  insertBefore(value, newVal) {
+    let newNode = new Node(newVal);
+    if (this.head) {
+      let current = this.head;
+      if (current.value === value) {
+        newNode.next = current;
+        this.head = newNode;
+        return;
+      }
+      while (current.next) {
+        if (current.next.value === value) {
+          let temp = current.next;
+          current.next = newNode;
+          newNode.next = temp;
+
+          return current.value;
+        }
+        current = current.next;
+      }
+    } else {
+      return `Exception`;
+    }
+
+  }
 }
 module.exports = LinkedList;
 
