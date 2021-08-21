6dec5923f01ffbe768077fd2c6f78006cbbde471
En-ZUH <enas_z_bataineh108@yahoo.com>
2021-08-21 04:50:47 +0300

    done

---
+++ javascript/Data Structure-401/HashTable/Hashmap Tree Intersection/README.md
@@ -0,0 +1,7 @@
+# Hashmap Tree Intersection
+
+## whiteboard 
+ ![img](./assets/challenge32-board.jpg)
+
+ ## Testing 
+ ![img](./assets/challenge32-test.PNG)
---
+++ javascript/Data Structure-401/HashTable/Hashmap Tree Intersection/hashmap-tree-intersection.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const { HashTable } = require('../hash table/hashTable');
+
+const BT = require('../../Tree/Binary Tree/tree').Binary_Tree;
+const Node = require('../../Tree/Binary Tree/tree').Node;
+
+
+function treeIntersection(tree_1, tree_2) {
+    if (tree_1.root === null || tree_2.root === null) {
+        return 'Empty Tree!';
+    }
+    let arr = [];
+    const newHashTable = new HashTable(3533);
+    let arr_1 = tree_1.preOrder();
+    let arr_2 = tree_2.preOrder();
+
+    for (let i = 0; i < arr_1.length; i++) {
+        let key = arr_1[i].toString();
+        let val = arr_1[i];
+        if (!newHashTable.contains(key)) {
+            newHashTable.add(key, val);
+        }
+    }
+    for (let i = 0; i < arr_2.length; i++) {
+        let key = arr_2[i].toString();
+        let val2 = arr_2[i];
+        if (newHashTable.contains(key)) {
+            arr.push(val2);
+        }
+    }
+    if (arr.length > 0) {
+        return arr;
+    } else {
+        return 'There is No matching values';
+    }
+}
+
+module.exports = treeIntersection;
+
+
